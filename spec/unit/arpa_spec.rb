87e2e55ec4672369e44fe0e6344a99e4199ca332
Justen Walker <justen.walker+github@gmail.com>
2014-01-06 12:05:41 -0500

    Remove dummy spec from arpa_spec

--- spec/unit/arpa_spec.rb
+++ spec/unit/arpa_spec.rb
@@ -19,14 +19,11 @@ describe RedZone::Arpa do
     subject { arpa_ipv4 }
     its(:name) { should == '32.12.in-addr.arpa'}
     its(:network) { should == IPAddr.new('12.32.0.0/16') }
-    its ("to_s") { should eq("") }
-
   end
   context "ipv6 network" do
     subject { arpa_ipv6 }
     its (:name)    { should == '9.8.7.6.4.3.2.1.1.0.0.2.ip6.arpa' }
     its (:network) { should == IPAddr.new('2001:1234:6789::/48') }
-    it { subject.to_s.should eq("") }
   end
   it 'should require name option' do
     expect { described_class.new({:network => '12.32.0.0/16', :soa => 'soa'})}.to raise_error(ArgumentError,':name is required')
