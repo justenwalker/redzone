03c0bb50404a04c7e127e8d5f2cfc184fb5d9390
Justen Walker <justen.walker+github@gmail.com>
2014-01-06 12:26:27 -0500

    Add nxdomain ttl to SOA
    SOA used to use the default TTL for the nxdomain ttl.
    Now it uses the lifetimes: negative value instead.

--- lib/redzone/soa.rb
+++ lib/redzone/soa.rb
@@ -17,6 +17,10 @@ module RedZone
   #
   class SOA
     # Returns a new instance of a zone SOA record
+    #
+    # See [SOA reference](http://www.zytrax.com/books/dns/ch8/soa.html) 
+    # for explaination of the fields.
+    #
     # @param [Hash] opts
     # @option opts [String] :domain     Zone (domain) name
     # @option opts [String] :ns         Primary name server
@@ -24,15 +28,17 @@ module RedZone
     # @option opts [String] :refresh    Time between refreshes from slave servers
     # @option opts [String] :retry      Time between retrying failed zone transfers
     # @option opts [String] :expire     The maximum time that a secondary server will keep trying to complete a zone transfer.
+    # @option opts [String] :negative   The maximum negative caching time.
     # @option opts [String] :ttl        The minimum time-to-live that applies to all resource records in the zone file
     def initialize(opts)
       @domain     = opts[:domain]
       @ns         = opts[:ns]
-      @hostmaster = escape(opts[:hostmaster]   || "hostmaster.#{@domain}") 
-      @ttl        = Lifetime.new(opts[:ttl]     || '1H')
-      @refresh    = Lifetime.new(opts[:refresh] || '1H')
-      @retry      = Lifetime.new(opts[:retry]   || '10M')
-      @expire     = Lifetime.new(opts[:expire]  || '1D')
+      @hostmaster = escape(opts[:hostmaster]     || "hostmaster.#{@domain}") 
+      @ttl        = Lifetime.new(opts[:ttl]      || '1H')
+      @refresh    = Lifetime.new(opts[:refresh]  || '1H')
+      @retry      = Lifetime.new(opts[:retry]    || '10M')
+      @expire     = Lifetime.new(opts[:expire]   || '1D')
+      @negative   = Lifetime.new(opts[:negative] || '1H')
     end
     # Returns the SOA record with the serial set to the current unix timestamp
     # @return [String] SOA Record
@@ -52,7 +58,7 @@ module RedZone
       io << "               %-10s ; ref = refresh = %s\n"  % [@refresh.seconds,@refresh] 
       io << "               %-10s ; rt  = retry   = %s\n"  % [@retry.seconds,@retry]
       io << "               %-10s ; ex  = expiry  = %s\n"  % [@expire.seconds,@expire]
-      io << "               %-10s ; ttl = %s\n"  % [@ttl.seconds,@ttl]
+      io << "               %-10s ; nx  = nxdomain ttl = %s\n"  % [@negative.seconds,@negative]
       io << ")\n\n"
       io.string
     end
